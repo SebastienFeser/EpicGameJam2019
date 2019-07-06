c9caf47a74bb85a66e28637f6c2582496136ec54
Sebastien Feser <sedukafe@gmail.com>
2019-07-06 09:52:49 +0200

    Mobs movements

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigidBody;
+    [SerializeField] PlayerTypes type;
+    [SerializeField] float speedMultiplier;
+
+    enum PlayerTypes
+    {
+        FARMER,
+        BUNNY,
+        FOX,
+        CHICKEN
+    }
+    
+
+    private void Update()
+    {
+        rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speedMultiplier, Input.GetAxisRaw("Vertical") * speedMultiplier);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+}
